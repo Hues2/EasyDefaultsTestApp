9992b993db1a406b4e0441a99ca2d21c58c2af71
Greg Ross <gregandrewross79@gmail.com>
2024-08-11 12:01:00 +0100

    Initial commit

---
+++ EasyDefaultsTestApp/ContentView.swift
@@ -0,0 +1,69 @@
+//
+//  ContentView.swift
+//  EasyDefaultsTestApp
+//
+//  Created by Greg Ross on 11/08/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State var counter = UserDefaults.standard.integer(forKey: "counter") + 1
+    var body: some View {
+        VStack {
+            Text("Use this app bundle ID in the Easy Defaults app: com.test.SaveToDefaultsApp\n\nAnd select the simulator that is running this test app.")
+                .font(.title2)
+                .fontWeight(.regular)
+                .padding(.vertical)
+                .frame(maxHeight: .infinity)
+            Button {
+                UserDefaults.standard.setValue("Easy Defaults Test Value", forKey: "\(counter)")
+                counter += 1
+            } label: {
+                Text("Save to User Defaults")
+                    .foregroundStyle(.white)
+                    .font(.title)
+                    .fontWeight(.black)
+                    .padding()
+                    .frame(maxWidth: .infinity)
+                    .background(Color.green)
+                    .clipShape(.rect(cornerRadius: 12))
+            }
+            .buttonStyle(ScaleButtonStyle())
+            
+            Button {
+                clearUserDefaults()
+            } label: {
+                Text("Clear User Defaults")
+                    .foregroundStyle(.white)
+                    .font(.title)
+                    .fontWeight(.black)
+                    .padding()
+                    .frame(maxWidth: .infinity)
+                    .background(Color.pink)
+                    .clipShape(.rect(cornerRadius: 12))
+            }
+            .buttonStyle(ScaleButtonStyle())
+        }
+        .padding()
+    }
+    
+    func clearUserDefaults() {
+        let defaults = UserDefaults.standard
+        if let bundleID = Bundle.main.bundleIdentifier {
+            defaults.removePersistentDomain(forName: bundleID)
+            defaults.synchronize()
+        }
+    }
+}
+
+struct ScaleButtonStyle : ButtonStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+            .scaleEffect(configuration.isPressed ? 0.8 : 1)
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ EasyDefaultsTestApp/EasyDefaultsTestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  EasyDefaultsTestAppApp.swift
+//  EasyDefaultsTestApp
+//
+//  Created by Greg Ross on 11/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct EasyDefaultsTestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
